395322c1d17fdb7e4cdef046bebfa969084370c8
Jeannie Kim <SJ.Jeannie.Kim@gmail.com>
2018-10-03 11:24:35 -0500

    I want to work at Bob's Burgers and get paid 00K a month.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Transaction = () => {
+const Transaction = (props) => {
+  // console.log(props);
   return (
     <tr>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
+      <td>{props.transaction.posted_at}</td>
+      <td>{props.transaction.description}</td>
+      <td>{props.transaction.category}</td>
+      <td>${props.transaction.amount}</td>
     </tr>
-  )
-}
+  );
+};
 
-export default Transaction
+//       posted_at: '2017-02-29 10:30:00',
+//       description: "Paycheck from Bob's Burgers",
+//       category: 'Income',
+//       amount: 100000
+
+export default Transaction;

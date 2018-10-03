15e855f18290005bf05ca99e5148a03431079e2f
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-10-03 11:30:14 -0500

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -11,19 +11,32 @@ class AccountContainer extends Component {
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
+    this.state = {
+      transactions: [],
+      searchTerm: ""
+    }
 
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount = () => {
+    this.setState({
+      transactions: transactions
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
   }
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList
+          transactions={this.state.transactions}
+          searchTerm={this.state.searchTerm}/>
       </div>
     )
   }
